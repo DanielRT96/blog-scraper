163fdd333f4b18ac696859266d303f4f80204489
DanielRT96 <reti.daniel96@gmail.com>
2021-04-21 20:34:38 +0200

    update BlogContainer

--- client/src/components/BlogContainer/BlogContainer.js
+++ client/src/components/BlogContainer/BlogContainer.js
@@ -1,7 +1,14 @@
+import React from 'react';
+
 import './BlogContainer.css';
+import BlogItem from './BlogItem/BlogItem';
 
 const BlogContainer = () => {
-  return <div className="blog__container"></div>;
+  return (
+    <div className="blog__container">
+      <BlogItem></BlogItem>
+    </div>
+  );
 };
 
 export default BlogContainer;
