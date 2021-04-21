3e216d588a03873eae981cceb9789ac58eb83b19
DanielRT96 <reti.daniel96@gmail.com>
2021-04-21 20:34:12 +0200

    Update blogItem.js

--- client/src/components/BlogContainer/BlogItem/BlogItem.js
+++ client/src/components/BlogContainer/BlogItem/BlogItem.js
@@ -1,1 +1,13 @@
-// TODO
+import React from 'react';
+
+import './BlogItem.css';
+
+const BlogItem = (props) => {
+  return (
+    <a href={props.link} className="blog__item">
+      {props.name}
+    </a>
+  );
+};
+
+export default BlogItem;
